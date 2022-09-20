00414fe61f4975eb827a115b8063173439158d49
Gleyson Santos <gleysonsantos@protonmail.com>
2022-09-20 06:30:41 -0300

    Exemplo de Template

--- recipes/urls.py
+++ recipes/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from recipes.views import my_home, contato, sobre
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
+    path('', my_home),  # home
+    path('sobre/', sobre),  # sobre
+    path('contato/', contato),  # contato
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def my_home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Gleyson Santos',
+    })
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+
+def contato(request):
+    return render(request, 'temp.html')
