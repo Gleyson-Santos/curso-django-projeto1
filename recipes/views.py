e6185d091f0f529b0e0b9f91418b5a00690014f6
Gleyson Santos <gleysonsantos@protonmail.com>
2022-09-21 06:59:29 -0300

    Early changes

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={
-        'name': 'Luiz Otávio',
+        'name': 'Gleyson Santos',
     })
 
 
