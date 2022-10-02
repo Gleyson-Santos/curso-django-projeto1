9073c7bc868b6f46f011a66a94e814952a1d78cf
Gleyson Santos <gleysonsantos@protonmail.com>
2022-10-02 10:45:24 -0300

    02 out

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 template_home = 'recipes/pages/home.html'
 template_recipe = 'recipes/pages/recipe-view.html'
 
+context_home = {
+    'recipes': [make_recipe() for _ in range(10)]
+}
+
+context_recipe = {
+    'recipe': make_recipe()
+}
+
 
 def home(request):
-    return render(request, template_home)
+    return render(request, template_home, context_home)
 
 
 def recipe(request, id):
-    return render(request, template_recipe)
+    return render(request, template_recipe, context_recipe)
